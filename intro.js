6b07ab6f1fd4dd7e52916dd1d2572d6f1fe1c716
Spencer Wadsworth <wadzy1@outlook.com>
2020-01-14 07:55:22 -0700

    Code written Jan 13 2020

---
+++ intro.js
@@ -0,0 +1,12 @@
+// alert('Hello World');
+var mySum = addValues(2, 2);
+// console.log(mySum);
+
+var headerElement = document.getElementById('header');
+headerElement.innerText = mySum;
+function addValues(first, second) {
+  var sum = first + second;
+  return sum;
+}
+
+sum = 6;
