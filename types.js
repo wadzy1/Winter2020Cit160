f35dc41b0eb63df8cd0afa755d0c7cf2ea91fbfe
Spencer Wadsworth <wadzy1@outlook.com>
2020-01-27 15:45:40 -0700

    Exam 1 Prep

---
+++ types.js
@@ -0,0 +1,49 @@
+var fullName = 'Ron Swanson';
+
+var age = 50;
+var age2 = '50';
+age2 == age;
+age2 === age;
+(age++ && !age2) || age;
+
+var isAdmin = true;
+parseFloat(age2);
+var person = {
+  name: fullName,
+  age: age,
+  isAdmin: isAdmin
+};
+
+var musicGenres = ['Country', 'Classical', 'Rock', 'Pop'];
+
+function greetUser(person) {
+  console.log('Hello ' + person.age);
+}
+17c^2
+let p2 = 3 * 2 - 17*(c * c) / (9 + 5);
+
+doSomething(param1)
+
+
+
+let swap = a;
+ a = b;
+ b = swap;
+
+ let p = 3 * 2 - (17 * Math.pow(c, 10)) / (9 + 5);
+ let p2 = 3 * 2 - 17 * 2 * 2 / (9 + 5);
+let x = 0;
+// Returns value at 0 index ++ is applied after 
+// the value of x is evaluated
+ myArray[x++]
+ x = 0
+ // Returns value in at index 1 the ++ is applied
+ // before the value of x is used
+ myArray[++x]
+ x++
+ x += 1
+ x = x + 1
+  
+
+
+
